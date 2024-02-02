955c94985f9bc84eb9ccfb8664b511689a531f11
jrfiii <rob.frank.iii@gmail.com>
2024-02-02 09:06:16 -0600

    end of day 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
-import path from 'path';
+/* eslint-disable @typescript-eslint/no-var-requires */
+// webpack will throw error if 'import' statement used instead of require statement
+//but TypeScript will use ES6 modules in rest of project, so just disabled the rule for this file
+
+const path = require('path');
 
 module.exports = {
-	entry: './src/client/index.tsx',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, './src/client/dist'),
-	},
+  mode: 'development',
+  watch: true,
+  entry: './src/client/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './src/client/dist'),
+  },
 };
