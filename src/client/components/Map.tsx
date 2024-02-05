bb13c913ddfa3a2167286bc5b3dafdc3556f5304
nwcassiani <nwcassiani@gmail.com>
2024-02-05 12:29:09 -0600

    Add basic Nav, Camera, Icon, Map, and Tours components

---
+++ src/client/components/Map.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { JsxE } from 'typescript';
+
+function Map():JSX.Element {
+  return (
+    <div>
+      <h1>Map</h1>
+    </div>
+  );
+}
+
+export default Map;
