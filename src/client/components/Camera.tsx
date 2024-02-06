79efe69cac2882d93f97ea5c687a4b06e7903e32
nwcassiani <nwcassiani@gmail.com>
2024-02-05 20:00:58 -0600

    Add capture attr to camera

--- src/client/components/Camera.tsx
+++ src/client/components/Camera.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 
+// make state photo variable that holds photo file
 function Camera():JSX.Element {
+
+  // function to access photo data
+  // check for env or user id
+
+  const fileInput = document.getElementById('environment');
+
+  const sendPic = () => {
+    // let file = fileInput.files[0];
+    console.log(fileInput);
+ 
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    sendPic();
+  };
+
   return (
     <div>
-      <h1>Camera</h1>
+      <label htmlFor="environment">Capture environment:</label>
+      <br />
+      <input
+        type="file"
+        id="environment"
+        capture="environment"
+        accept="image/*"
+        onChange={handleChange}
+      />
+      <br />
+      <label htmlFor="user">Capture user:</label>
+      <br />
+      <input
+        type="file"
+        id="user"
+        capture="user"
+        accept="image/*"
+      />
     </div>
   );
 }
