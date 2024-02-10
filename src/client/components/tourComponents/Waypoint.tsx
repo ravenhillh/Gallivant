9898207696cf1cf3587391e5f7ca44373e2ce6c5
jrfiii <rob.frank.iii@gmail.com>
2024-02-10 15:28:35 -0600

    add edit modal and state variables back to waypoint component for putting edits

--- src/client/components/tourComponents/Waypoint.tsx
+++ src/client/components/tourComponents/Waypoint.tsx
@@ -18,23 +18,46 @@ interface WaypointProps {
 
 const Waypoint = (props: WaypointProps): JSX.Element => {
   const { waypoint, id_tour, getTourWPs } = props;
-  const [modal, setModal] = useState<boolean>(false);
-  // const [name, setName] = useState<string>('');
-  // const [description, setDescription] = useState<string>('');
+  const [delModal, setDelModal] = useState<boolean>(false);
+  const [editModal, setEditModal] = useState<boolean>(false);
 
-  // const handleChange = (
-  //   event: React.ChangeEvent<HTMLInputElement>,
-  //   setState: React.Dispatch<string>
-  // ) => {
-  //   setState(event.target.value);
-  // };
+  const [waypointName, setName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    setState: React.Dispatch<string>
+  ) => {
+    setState(event.target.value);
+  };
+
+  const openEditModal = () => {
+    setEditModal(true);
+    setName(waypoint.waypointName);
+    setDescription(waypoint.description);
+  };
+
+  const editWaypoint = (wpId: number) => {
+    axios.put(`/db/waypoint/${wpId}`, {
+      waypointName, description
+    })
+    .then((res) => {
+      if (res.status === 200) {
+        setEditModal(false);
+        getTourWPs(id_tour);
+      }
+    })
+    .catch((err: string) =>
+      console.error('Could not EDIT waypoint: ', err)
+    );
+  };
 
   const deleteWaypoint = (wpId: number) => {
     axios
       .delete(`/db/waypoint/${wpId}/${id_tour}`)
       .then((res) => {
         if (res.status === 200) {
-          setModal(false);
+          setDelModal(false);
           getTourWPs(id_tour);
         }
       })
@@ -50,23 +73,41 @@ const Waypoint = (props: WaypointProps): JSX.Element => {
       <div>
         Long: {waypoint.long}, Lat: {waypoint.lat}
       </div>
-      <button onClick={() => setModal(true)}>🗑️</button>
-      <Modal openModal={modal} closeModal={() => setModal(false)}>
+
+      <button onClick={() => setDelModal(true)}>🗑️</button>
+      <Modal
+        className='delete-waypoint-modal'
+        openModal={delModal}
+        closeModal={() => setDelModal(false)}
+      >
         <div>Are you sure you want to delete Waypoint?</div>
         <button onClick={() => deleteWaypoint(waypoint.id)}>
           Delete waypoint
         </button>
       </Modal>
-      {/* <input
-        type='text'
-        placeholder='Give the waypoint a name'
-        onChange={(e) => handleChange(e, setName)}
-      />
-      <input
-        type='text'
-        placeholder='Describe the place'
-        onChange={(e) => handleChange(e, setDescription)}
-      /> */}
+
+      <button onClick={openEditModal}>Edit</button>
+      <Modal
+        className='edit-waypoint-modal'
+        openModal={editModal}
+        closeModal={() => setEditModal(false)}
+      >
+        <input
+          type='text'
+          value={waypointName}
+          placeholder='Give the waypoint a name'
+          onChange={(e) => handleChange(e, setName)}
+        />
+        <input
+          type='text'
+          value={description}
+          placeholder='Describe the place'
+          onChange={(e) => handleChange(e, setDescription)}
+        />
+        <button onClick={() => editWaypoint(waypoint.id)}>
+          Edit waypoint
+        </button>
+      </Modal>
     </li>
   );
 };
