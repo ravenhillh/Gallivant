ed617246c5c6b68a82d455363a45bc8896410b40
jrfiii <rob.frank.iii@gmail.com>
2024-02-10 14:12:13 -0600

    add delete route for waypoint

--- src/server/routes/waypoints.ts
+++ src/server/routes/waypoints.ts
@@ -47,4 +47,19 @@ waypointRouter.post('/db/waypoint/', async (req, res) => {
   //
 });
 
+waypointRouter.delete('/db/waypoint/:wp/:tour', async (req, res) => {
+  const { wp, tour } = req.params;
+  console.log('id_tour: ', tour, 'id_waypoint: ', wp);
+
+  Tours_Waypoints.destroy({ where: { id_tour: tour, id_waypoint: wp } })
+    .then(async () => {
+      await Waypoint.destroy({ where: { id: wp } });
+      res.sendStatus(200);
+    })
+    .catch((err: string) => {
+      console.error('Failed to delete waypoint: ', err);
+      res.sendStatus(500);
+    });
+});
+
 export default waypointRouter;
