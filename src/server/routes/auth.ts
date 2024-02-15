0de9bc0619ad7a910168ce59866590652e115c86
jrfiii <rob.frank.iii@gmail.com>
2024-02-15 14:36:16 -0600

    re-enable session authentication for tour component, to create conditional rendering of edit/delete features

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -52,7 +52,8 @@ passport.deserializeUser(function ([userInstance, created], cb) {
 // client side authentication check for protected component loaders
 authRouter.get('/auth/client', (req: Request, res: Response) => {
   const verify: boolean = req.isAuthenticated();
-  res.status(200).send(verify);
+  const authObj = { verify, user: req.user };
+  res.status(200).send(authObj);
 });
 
 // Google authentication and redirect callback
