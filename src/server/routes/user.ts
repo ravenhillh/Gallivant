d4b8b0e2ff71d87228c73820adc4fa9c03faf455
jrfiii <rob.frank.iii@gmail.com>
2024-02-23 14:37:54 -0600

    write route for updating id_currentTour for User

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { User } from '../db/index';
 
+// ALL ROUTE PATHS PRECEDED BY '/user'
 const userRouter = express.Router();
 
 // GET user by id
@@ -8,12 +9,22 @@ userRouter.get('/:id', (req, res) => {
   const { id } = req.params;
 
   User.findByPk(id)
-    .then((data:object) => {
+    .then((data: object) => {
       // console.log(data);
       res.send(data).status(200);
     })
-    .catch((err:string) => {
+    .catch((err: string) => {
       console.error('Could not GET user data ', err);
+      res.sendStatus(500);
+    });
+});
+
+userRouter.put('/:userId/:tourId', (req, res) => {
+  const { userId, tourId } = req.params;
+  User.update({ id_currentTour: tourId }, { where: { id: userId } })
+    .then(() => res.sendStatus(200))
+    .catch((err: string) => {
+      console.error('Failed to Update id_currentTour of User: ', err);
       res.sendStatus(500);
     });
 });
