ab5bdd7e8ac1a3db7e88018474bbf284b6fc8063
jrfiii <rob.frank.iii@gmail.com>
2024-02-22 20:19:57 -0600

    convert voice component to FormControl with InputAdornment instead of Fab button for MicIcon

--- src/client/components/tourComponents/Voice.tsx
+++ src/client/components/tourComponents/Voice.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import useSpeechToText from '../../utils/speechToText';
 
-import { TextField, Fab, MicIcon, MicOffIcon } from '../../utils/material';
+import {
+  TextField,
+  Fab,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  IconButton,
+  MicIcon,
+  MicOffIcon,
+} from '../../utils/material';
+import { FormHelperText } from '@mui/material';
 
-const Voice = ({ textInput, setTextInput, type, label, helperText }): JSX.Element => {
+const Voice = ({
+  textInput,
+  setTextInput,
+  type,
+  label,
+  helperText,
+}): JSX.Element => {
   const { isListening, transcript, startListening, stopListening } =
     useSpeechToText();
 
@@ -21,31 +38,44 @@ const Voice = ({ textInput, setTextInput, type, label, helperText }): JSX.Elemen
   };
 
   return (
-    <div>
-      <TextField
-        disabled={isListening}
-        autoFocus={type === 'name'}
-        multiline={type === 'description'}
-        label={label}
-        helperText={helperText}
-        onChange={(e) => setTextInput(e.target.value)}
-        value={
-          isListening
-            ? textInput +
-              (transcript.length
-                ? (textInput.length ? ' ' : '') + transcript
-                : '')
-            : textInput
-        }
-      />
-      <Fab
-        size='small'
-        color={isListening ? 'warning' : 'success'}
-        onClick={() => startStopListening()}
-      >
-        {isListening ? <MicOffIcon /> : <MicIcon />}
-      </Fab>
-    </div>
+      <FormControl sx={{ m: 1, width: '25ch' }} variant='outlined'>
+        <InputLabel htmlFor='outlined-adornment-label'>{label}</InputLabel>
+        <OutlinedInput
+          id={`outlined-adornment-${label}`}
+          type='text'
+          fullWidth
+          disabled={isListening}
+          autoFocus={type === 'name'}
+          multiline={type === 'description'}
+          // helperText={helperText}
+          onChange={(e) => setTextInput(e.target.value)}
+          value={
+            isListening
+              ? textInput +
+                (transcript.length
+                  ? (textInput.length ? ' ' : '') + transcript
+                  : '')
+              : textInput
+          }
+          endAdornment={
+            <InputAdornment position='end'>
+              <IconButton
+                aria-label='toggle speech to text'
+                color={isListening ? 'warning' : 'success'}
+                onClick={startStopListening}
+                onMouseDown={startStopListening}
+                edge='end'
+              >
+                {isListening ? <MicOffIcon /> : <MicIcon />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label={label}
+        />
+        <FormHelperText>
+          {helperText}
+        </FormHelperText>
+      </FormControl>
   );
 };
 
