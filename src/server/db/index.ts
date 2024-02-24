fee3db8ea3570516adf0a5255583d2aeed074a31
jrfiii <rob.frank.iii@gmail.com>
2024-02-24 10:01:36 -0600

    change to db/index for simpler console.log toggling

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -6,54 +6,63 @@ dotenv.config();
 const sqlPw: string = process.env.SQL_PW ?? '';
 
 const db = new Sequelize('gallivant', 'root', sqlPw, {
-    host: 'localhost',
-    dialect: 'mysql'
+  host: 'localhost',
+  dialect: 'mysql',
 });
 
-
 db.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch((error: string) => {
-        console.error('Unable to connect to the database:', error);
-    });
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error: string) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
+db.options.logging = false;
+db.sync();
+db.options.logging = console.log;
+// db.options.logging = (...msg: any) => console.log(msg);
 
 // CREATED USERS TABLE
-const User = db.define('User', {
+const User = db.define(
+  'User',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     username: DataTypes.STRING,
     googleId: DataTypes.STRING,
     currentPosition: DataTypes.INTEGER,
     id_currentTour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'Tours',
-            key: 'id',
-            allowNull: false
-        }
-    }
-}, { timestamps: true });
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tours',
+        key: 'id',
+        allowNull: false,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 // CREATED TOURS TABLE
-const Tour = db.define('Tour', {
+const Tour = db.define(
+  'Tour',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     id_createdByUser: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-            allowNull: false
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+        allowNull: false,
+      },
     },
     category: DataTypes.STRING,
     ratingAvg: DataTypes.DECIMAL,
@@ -63,363 +72,383 @@ const Tour = db.define('Tour', {
     neighborhood: DataTypes.STRING,
     completions: DataTypes.INTEGER,
     totalWaypoints: DataTypes.INTEGER,
-    startLong: DataTypes.DECIMAL(7,5),
-    startLat: DataTypes.DECIMAL(7,5)
-}, { timestamps: true });
-
+    startLong: DataTypes.DECIMAL(7, 5),
+    startLat: DataTypes.DECIMAL(7, 5),
+  },
+  { timestamps: true }
+);
 
 // CREATED WAYPOINTS TABLE
-const Waypoint = db.define('Waypoint', {
+const Waypoint = db.define(
+  'Waypoint',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     waypointName: DataTypes.STRING,
     description: DataTypes.TEXT,
     prompt: DataTypes.TEXT,
     answer: DataTypes.STRING,
-    long: DataTypes.DECIMAL(7,5),
-    lat: DataTypes.DECIMAL(7,5),
-    ratingAvg: DataTypes.DECIMAL
-}, { timestamps: true });
+    long: DataTypes.DECIMAL(7, 5),
+    lat: DataTypes.DECIMAL(7, 5),
+    ratingAvg: DataTypes.DECIMAL,
+  },
+  { timestamps: true }
+);
 
 // CREATED CATEGORIES TABLE
-const Category = db.define('Category', { //waypoint description
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    category: DataTypes.STRING,
-    icon: DataTypes.STRING
+const Category = db.define('Category', {
+  //waypoint description
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  category: DataTypes.STRING,
+  icon: DataTypes.STRING,
 });
 
 // CREATED IMAGES TABLE
-const Image = db.define('Image', {
+const Image = db.define(
+  'Image',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     id_user: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-            allowNull: false
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+        allowNull: false,
+      },
     },
     thumbnail: DataTypes.STRING,
-    largeImg: DataTypes.STRING
-}, { timestamps: true });
+    largeImg: DataTypes.STRING,
+  },
+  { timestamps: true }
+);
 
 // CREATED REVIEWS TABLE
-const Review = db.define('Review', {
+const Review = db.define(
+  'Review',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     id_user: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-            allowNull: false
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+        allowNull: false,
+      },
     },
     id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tour,
+        key: 'id',
+        allowNull: false,
+      },
     },
     feedback: DataTypes.STRING,
-    rating: DataTypes.INTEGER
-}, { timestamps: true });
+    rating: DataTypes.INTEGER,
+  },
+  { timestamps: true }
+);
 
 // CREATED CHATS TABLE
-const Chat = db.define('Chat', {
+const Chat = db.define(
+  'Chat',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     id_user: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-            allowNull: false
-        }
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+        allowNull: false,
+      },
     },
-    message: DataTypes.STRING
-}, { timestamps: true });
+    message: DataTypes.STRING,
+  },
+  { timestamps: true }
+);
 
-const Users_Waypoints = db.define('Users_Waypoints', {
+const Users_Waypoints = db.define(
+  'Users_Waypoints',
+  {
     id_user: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+        allowNull: false,
+      },
+      onDelete: 'CASCADE',
     },
     id_waypoint: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Waypoint,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+      type: DataTypes.INTEGER,
+      references: {
+        model: Waypoint,
+        key: 'id',
+        allowNull: false,
+      },
+      onDelete: 'CASCADE',
     },
-    status: DataTypes.STRING
-  }, { timestamps: true });
+    status: DataTypes.STRING,
+  },
+  { timestamps: true }
+);
 
-const Images_Waypoints = db.define('Images_Waypoints', {
+const Images_Waypoints = db.define(
+  'Images_Waypoints',
+  {
     id_waypoint: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Waypoint,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+      type: DataTypes.INTEGER,
+      references: {
+        model: Waypoint,
+        key: 'id',
+        allowNull: false,
+      },
+      onDelete: 'CASCADE',
     },
     id_image: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Image,
-            key: 'id',
-            allowNull: false,
-        },
-        onDelete: 'CASCADE'
+      type: DataTypes.INTEGER,
+      references: {
+        model: Image,
+        key: 'id',
+        allowNull: false,
+      },
+      onDelete: 'CASCADE',
     },
-  }, { timestamps: true });
-
+  },
+  { timestamps: true }
+);
 
 const Waypoints_Categories = db.define('Waypoints_Categories', {
-    id_waypoint: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Waypoint,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+  id_waypoint: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Waypoint,
+      key: 'id',
+      allowNull: false,
     },
-    id_category: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Category,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_category: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Category,
+      key: 'id',
+      allowNull: false,
     },
+    onDelete: 'CASCADE',
+  },
 });
 
-
 const Tours_Waypoints = db.define('Tours_Waypoints', {
-    id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+  id_tour: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tour,
+      key: 'id',
+      allowNull: false,
     },
-    id_waypoint: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Waypoint,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_waypoint: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Waypoint,
+      key: 'id',
+      allowNull: false,
     },
-    order: DataTypes.INTEGER
-  });
+    onDelete: 'CASCADE',
+  },
+  order: DataTypes.INTEGER,
+});
 
-  const Tours_Categories = db.define('Tours_Categories', {
-    id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+const Tours_Categories = db.define('Tours_Categories', {
+  id_tour: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tour,
+      key: 'id',
+      allowNull: false,
     },
-    id_waypoint: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Category,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_waypoint: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Category,
+      key: 'id',
+      allowNull: false,
     },
-  });
+    onDelete: 'CASCADE',
+  },
+});
 
-const Completed_Tours = db.define('Completed_Tours', {
+const Completed_Tours = db.define(
+  'Completed_Tours',
+  {
     id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tour,
+        key: 'id',
+        allowNull: false,
+      },
+      onDelete: 'CASCADE',
     },
     id_user: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+      type: DataTypes.INTEGER,
+      references: {
+        model: User,
+        key: 'id',
+        allowNull: false,
+      },
+      onDelete: 'CASCADE',
     },
-  }, { timestamps: true });
-
+  },
+  { timestamps: true }
+);
 
 const Images_Reviews = db.define('Images_Reviews', {
-    id_review: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Review,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+  id_review: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Review,
+      key: 'id',
+      allowNull: false,
     },
-    id_image: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Image,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_image: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Image,
+      key: 'id',
+      allowNull: false,
     },
-  });
+    onDelete: 'CASCADE',
+  },
+});
 
-const Images_Tours  = db.define('Images_Tours', {
-    id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+const Images_Tours = db.define('Images_Tours', {
+  id_tour: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tour,
+      key: 'id',
+      allowNull: false,
     },
-    id_image: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Image,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_image: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Image,
+      key: 'id',
+      allowNull: false,
     },
-  });
-
+    onDelete: 'CASCADE',
+  },
+});
 
 const Reviews_Tours = db.define('Reviews_Tours', {
-    id_review: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Review,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+  id_review: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Review,
+      key: 'id',
+      allowNull: false,
     },
-    id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_tour: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tour,
+      key: 'id',
+      allowNull: false,
     },
-  });
+    onDelete: 'CASCADE',
+  },
+});
 
 const Reviews_Waypoints = db.define('Reviews_Waypoints', {
-    id_review: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Review,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+  id_review: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Review,
+      key: 'id',
+      allowNull: false,
     },
-    id_waypoint: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Waypoint,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_waypoint: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Waypoint,
+      key: 'id',
+      allowNull: false,
     },
-  });
-
+    onDelete: 'CASCADE',
+  },
+});
 
 const Chats_Tours = db.define('Chats_Tours', {
-    id_chat: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Chat,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+  id_chat: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Chat,
+      key: 'id',
+      allowNull: false,
     },
-    id_tour: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: Tour,
-            key: 'id',
-            allowNull: false
-        },
-        onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
+  },
+  id_tour: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Tour,
+      key: 'id',
+      allowNull: false,
     },
-  });
-
-db.options.logging = false;
-db.sync();
-db.options.logging = console.log;
-// db.options.logging = (...msg: any) => console.log(msg);
+    onDelete: 'CASCADE',
+  },
+});
 
 export {
-    db,
-    Tour,
-    User,
-    Waypoint,
-    Category,
-    Image,
-    Review,
-    Chat,
-    Users_Waypoints,
-    Images_Waypoints,
-    Waypoints_Categories,
-    Tours_Waypoints,
-    Completed_Tours,
-    Images_Reviews,
-    Images_Tours,
-    Reviews_Tours,
-    Reviews_Waypoints,
-    Chats_Tours,
-    Tours_Categories
+  db,
+  Tour,
+  User,
+  Waypoint,
+  Category,
+  Image,
+  Review,
+  Chat,
+  Users_Waypoints,
+  Images_Waypoints,
+  Waypoints_Categories,
+  Tours_Waypoints,
+  Completed_Tours,
+  Images_Reviews,
+  Images_Tours,
+  Reviews_Tours,
+  Reviews_Waypoints,
+  Chats_Tours,
+  Tours_Categories,
 };
