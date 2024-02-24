98cec77e90c875e39454b439275884c78f37cc60
jrfiii <rob.frank.iii@gmail.com>
2024-02-23 18:33:42 -0600

    add user route to update user current position on new db field

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -19,12 +19,22 @@ userRouter.get('/:id', (req, res) => {
     });
 });
 
-userRouter.put('/:userId/:tourId', (req, res) => {
+userRouter.put('/startTour/:userId/:tourId', (req, res) => {
   const { userId, tourId } = req.params;
-  User.update({ id_currentTour: tourId }, { where: { id: userId } })
+  User.update({ id_currentTour: tourId, currentPosition: 0 }, { where: { id: userId } })
     .then(() => res.sendStatus(200))
     .catch((err: string) => {
       console.error('Failed to Update id_currentTour of User: ', err);
+      res.sendStatus(500);
+    });
+});
+
+userRouter.put('/putPosition/:userId/:tourId/:position', (req, res) => {
+  const { userId, tourId, position } = req.params;
+  User.update({ id_currentTour: tourId, currentPosition: position }, { where: { id: userId } })
+    .then(() => res.sendStatus(200))
+    .catch((err: string) => {
+      console.error('Failed to Update position of User: ', err);
       res.sendStatus(500);
     });
 });
