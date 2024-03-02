370deeed1216cccec72c47a21aa8c927d746d4fc
nwcassiani <nwcassiani@gmail.com>
2024-03-02 14:30:08 -0600

    Center Login

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -4,13 +4,18 @@ import {
   Button, 
   Card,
   CardContent,
+  CardMedia,
   Typography } from '../utils/material';
 
 function Login() {
   return (
     <div>
-      <Card className="login" sx={{ maxWidth: 345 }}>
+      <Card className="login" sx={{ maxWidth: 345, mx: 'auto', textAlign: 'center' }}>
         <CardContent>
+          {/* <img
+            src="/api/images/napoli.jpg"
+            width="300"
+          /> */}
           <Typography variant='h2'>Login to Gallivant</Typography>
           <Button variant='contained' id='login'>
              <a className='button google' href='/auth/google'>
