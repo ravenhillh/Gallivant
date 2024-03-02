dfe40ada4771649deb8d58a7738533fe037e8f97
nwcassiani <nwcassiani@gmail.com>
2024-03-02 11:13:24 -0600

    Center login

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-import { Button, Typography } from '../utils/material';
+import { 
+  Button, 
+  Card,
+  CardContent,
+  Typography } from '../utils/material';
 
 function Login() {
   return (
     <div>
-      <Typography variant='h2'>Login to Gallivant</Typography>
-      <Button variant='contained' id='login'>
-        <a className='button google' href='/auth/google'>
-          Sign in with Google
-        </a>
-      </Button>
+      <Card className="login" sx={{ maxWidth: 345 }}>
+        <CardContent>
+          <Typography variant='h2'>Login to Gallivant</Typography>
+          <Button variant='contained' id='login'>
+             <a className='button google' href='/auth/google'>
+            Sign in with Google
+             </a>
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
