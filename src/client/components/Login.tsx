73501d7269b9006a197d0d9618bf375d18a3d873
nwcassiani <nwcassiani@gmail.com>
2024-03-02 17:04:10 -0600

    login housekeeping

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -2,28 +2,17 @@ import React from 'react';
 
 import { 
   Button, 
-  Card,
-  CardContent,
-  CardMedia,
   Typography } from '../utils/material';
 
 function Login() {
   return (
     <div className="log">
-      {/* <Card className="login" sx={{ maxWidth: 400, mx: 'auto', textAlign: 'center' }}>
-        <CardContent> */}
-          {/* <img
-            src="/api/images/napoli.jpg"
-            width="300"
-          /> */}
           <Typography variant='h2'>Login to Gallivant</Typography>
           <Button variant='contained' id='login'>
              <a className='button google' href='/auth/google'>
             Sign in with Google
              </a>
           </Button>
-        {/* </CardContent>
-      </Card> */}
     </div>
   );
 }
