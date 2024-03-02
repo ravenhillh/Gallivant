dc18312e1e2a5e724bcccc111499e9fbfb4d1d8b
nwcassiani <nwcassiani@gmail.com>
2024-03-02 17:23:30 -0600

    add logo to login page

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { 
   Button, 
+  RouteOutlinedIcon,
   Typography } from '../utils/material';
 
 function Login() {
   return (
     <div className="log">
+          <RouteOutlinedIcon sx={{ fontSize: '72px'}}/>
           <Typography variant='h2'>Login to Gallivant</Typography>
           <Button variant='contained' id='login'>
              <a className='button google' href='/auth/google'>
