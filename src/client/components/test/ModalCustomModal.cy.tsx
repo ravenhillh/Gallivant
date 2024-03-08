13edbb9f4ca5a35c9d4d81fdcbca1031780ac79a
jrfiii <rob.frank.iii@gmail.com>
2024-03-08 10:10:25 -0600

    modal test file with a few superficial tests

--- src/client/components/test/ModalCustomModal.cy.tsx
+++ src/client/components/test/ModalCustomModal.cy.tsx
@@ -4,12 +4,34 @@ import CustomModal from '../tourComponents/Modal';
 describe('<CustomModal />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<CustomModal openModal={true} closeModal={() => {}} >Test input</CustomModal>);
+    cy.mount(
+      <CustomModal openModal={true} closeModal={() => {}}>
+        Test input
+      </CustomModal>
+    );
   });
 
   it('should have a cancel button', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<CustomModal openModal={true} closeModal={() => {}} >Test input</CustomModal>);
-    const cancelButton = cy.get('.MuiButtonBase-root');
+    cy.mount(
+      <CustomModal openModal={true} closeModal={() => {}}>
+        Test input
+      </CustomModal>
+    );
+    cy.get('.MuiButtonBase-root');
+  });
+
+  it('clicking the cancel button should close modal', () => {
+    const onChangeSpy = cy.spy().as('onChangeSpy');
+    cy.mount(
+      <CustomModal
+        openModal={true}
+        closeModal={onChangeSpy}
+      >
+        Test input
+      </CustomModal>
+    );
+    cy.get('.MuiButtonBase-root').click();
+    cy.get('@onChangeSpy').should('have.been.called');
   });
 });
